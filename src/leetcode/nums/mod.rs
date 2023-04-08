69900a74f4d974c0180f450eb9c58aa5218d127c
donkey55 <lvshuopeng@icloud.com>
2023-04-08 10:41:18 +0800

    Revert "fix: add nums"
    This reverts commit f44405cedf32e52934e2882403c0fde8ac4ff4d8.

--- src/leetcode/nums/mod.rs
+++ src/leetcode/nums/mod.rs
@@ -1,5 +1,3 @@
 pub mod T9;
 pub mod T231;
 pub mod T172;
-pub mod T476;
-pub mod T319;
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 use std::cell::RefCell;
 use std::rc::Rc;
-use leetcode::leetcode::nums::T319::{Solution};
+use leetcode::leetcode::hash_table::T18::{Solution};
 use leetcode::Post::Post;
 
 fn main() {
-    let res = Solution::bulb_switch(3);
+    let res = Solution::four_sum(vec![1000000000,1000000000,1000000000,1000000000], -294967296);
     println!("{:?}", res);
 
 }
