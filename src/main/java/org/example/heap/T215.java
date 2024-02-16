7def3fa7cc6921ae473bc459f329eca52c30da9a
lsp <yekond123@gmail.com>
2024-02-16 15:33:22 +0800

    fix: add desc

--- src/main/java/org/example/heap/T215.java
+++ src/main/java/org/example/heap/T215.java
@@ -2,6 +2,14 @@ package org.example.heap;
 
 import java.util.PriorityQueue;
 
+/**
+ * 第k大元素
+ * TopK，最小堆
+ * MinK，最大堆
+ * @Datetime: 2024/2/16 15:32
+ * @Author: koopkl
+ * @Link: <a href="https://leetcode.cn/problems/kth-largest-element-in-an-array">第k大元素</a>
+ */
 public class T215 {
     public int findKthLargest(int[] nums, int k) {
         PriorityQueue<Integer> maxHeap = new PriorityQueue<>(k);
